f41119b343473d79e9c0b4cdeec8dd7fdb5ee776
Mark Baldwin <baldwin.mv05@gmail.com>
2021-11-18 08:15:20 -0700

    node server modes

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,6 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    console.log('first middleware');
-    next();
-});
-
-app.use((req, res, next) => {
     res.send('second middleware');
 });
 
